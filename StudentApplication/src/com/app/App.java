341440ba481524abe4b1d49d70be9db061702318
kalpesh <kalpesh162@gmail.com>
2026-05-10 09:53:56 +0530

    all crud op 2

--- StudentApplication/src/com/app/App.java
+++ StudentApplication/src/com/app/App.java
@@ -1,9 +1,49 @@
 package com.app;
 
+import java.util.List;
+
+import com.dao.StudentDao;
+import com.dao.StudentDaoImpl;
+import com.entity.Student;
+
 public class App {
-	
+
 	public static void main(String[] args) {
-		
+		Student s1 = new Student(11, "Rahul", 99);
+		StudentDao studentDao = new StudentDaoImpl();
+		studentDao.saveStudent(s1);
+
+		System.out.println("Save Student");
+		System.out.println("+++++++++++++++++++++++++++++");
+
+		System.out.println("Read Student");
+		Student student = studentDao.getStudentById(11);
+		System.out.println(student);
+
+		System.out.println("+++++++++++++++++++++++++++++");
+
+		s1.setName("Kahul");
+		s1.setMarks(55.55);
+		System.out.println("Update ");
+		studentDao.updateStudent(student);
+		studentDao.updateStudent(s1);
+		System.out.println("+++++++++++++++++++++++++++++");
+		System.out.println("Read Student");
+		Student student1 = studentDao.getStudentById(11);
+		System.out.println(student1);
+
+		System.out.println("+++++++++++++++++++++++++++++");
+		System.out.println("DELETE");
+		studentDao.deleteStudent(s1);
+		System.out.println("----------------------");
+
+		System.out.println("Read ALL Students");
+
+		List<Student> list = studentDao.getAllStudent();
+
+		for (Student stud : list)
+			System.out.println(stud);
+
 	}
 
 }
--- StudentApplication/src/com/dao/StudentDaoImpl.java
+++ StudentApplication/src/com/dao/StudentDaoImpl.java
@@ -2,6 +2,7 @@ package com.dao;
 
 import java.util.List;
 
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -15,14 +16,14 @@ public class StudentDaoImpl implements StudentDao {
 	public void saveStudent(Student student) {
 		SessionFactory factory = HibernateUtility.getSessionFactory();
 		Session session = factory.openSession();
-		Transaction tx=null;
+		Transaction tx = null;
 		try {
-			tx=session.beginTransaction();
+			tx = session.beginTransaction();
 			session.save(student);
 			tx.commit();
-			
+
 		} catch (Exception e) {
-			if(tx!=null)
+			if (tx != null)
 				tx.rollback();
 			throw e;
 
@@ -34,32 +35,89 @@ public class StudentDaoImpl implements StudentDao {
 
 	@Override
 	public void updateStudent(Student student) {
-		// TODO Auto-generated method stub
+
+		SessionFactory factory = HibernateUtility.getSessionFactory();
+		Session session = factory.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			session.update(student);
+			tx.commit();
+
+		} catch (Exception e) {
+			if (tx != null)
+				tx.rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
 
 	}
 
 	@Override
 	public void deleteStudent(Student student) {
-		// TODO Auto-generated method stub
+		SessionFactory factory = HibernateUtility.getSessionFactory();
+		Session session = factory.openSession();
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			session.delete(student);
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null)
+				tx.rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
 
 	}
 
 	@Override
 	public void deleteStudentById(int id) {
-		// TODO Auto-generated method stub
+		SessionFactory factory = HibernateUtility.getSessionFactory();
+		Session session = factory.openSession();
+		Student student = new Student();
+		student.setId(id);
+		Transaction tx = null;
+		try {
+			tx = session.beginTransaction();
+			session.delete(student);
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null)
+				tx.rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
 
 	}
 
 	@Override
 	public Student getStudentById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		SessionFactory factory = HibernateUtility.getSessionFactory();
+		Session session = factory.openSession();
+		Student student = null;
+		try {
+			// student = (Student) session.get(Student.class, id);
+			student = (Student) session.load(Student.class, id);
+			student.getName();
+			return student;
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			session.close();
+		}
 	}
 
 	@Override
 	public List<Student> getAllStudent() {
-		// TODO Auto-generated method stub
-		return null;
+		SessionFactory factory = HibernateUtility.getSessionFactory();
+		Session session = factory.openSession();
+		String hql = "from Student s";
+		Query<Student> query = session.createQuery(hql);
+		return query.list();
 	}
 
 }
