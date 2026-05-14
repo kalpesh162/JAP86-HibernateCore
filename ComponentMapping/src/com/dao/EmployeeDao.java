198c5119a19c8f08eeef249268ff46df2fa69b3e
kalpesh <kalpesh162@gmail.com>
2026-05-14 16:05:52 +0530

    component mapping part -3

--- ComponentMapping/src/com/dao/EmployeeDao.java
+++ ComponentMapping/src/com/dao/EmployeeDao.java
@@ -1,9 +1,21 @@
 package com.dao;
 
+import java.util.List;
+
 import com.entity.Employee;
 
 public interface EmployeeDao {
-	
+
 	void saveEmployee(Employee employee);
+
+	void deleteEmployee(Employee employee);
+
+	void deleteEmployeeById(int id);
+
+	void updateEmployee(Employee employee);
+
+	Employee findById(int id);
+	
+	List<Employee> findAllEmployee();
 
 }
--- ComponentMapping/src/com/dao/EmployeeDaoImpl.java
+++ ComponentMapping/src/com/dao/EmployeeDaoImpl.java
@@ -1,5 +1,8 @@
 package com.dao;
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -26,6 +29,97 @@ public class EmployeeDaoImpl implements EmployeeDao {
 		} finally {
 			session.close();
 		}
+	}
+
+	@Override
+	public void deleteEmployee(Employee employee) {
+		SessionFactory factory = HibernateUtility.getSessionFactory();
+		Session session = factory.openSession();
+		Transaction tx = null;
+		try {
+
+			tx = session.beginTransaction();
+			session.delete(employee);
+			tx.commit();
+
+		} catch (Exception e) {
+			if (tx != null)
+				tx.rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
+	}
+
+	@Override
+	public void deleteEmployeeById(int id) {
+		SessionFactory factory = HibernateUtility.getSessionFactory();
+		Session session = factory.openSession();
+		Transaction tx = null;
+		Employee employee = findById(id);
+		try {
+			if (employee != null) {
+
+				tx = session.beginTransaction();
+				session.delete(employee);
+				tx.commit();
+			}
+
+		} catch (Exception e) {
+			if (tx != null)
+				tx.rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
+	}
+
+	public Employee findById(int id) {
+		SessionFactory factory = HibernateUtility.getSessionFactory();
+		Session session = factory.openSession();
+		Employee employee = null;
+		try {
+
+			employee = (Employee) session.get(Employee.class, id);
+			return employee;
+
+		} catch (Exception e) {
+
+			throw e;
+		} finally {
+			session.close();
+		}
+
+	}
+
+	@Override
+	public void updateEmployee(Employee employee) {
+		SessionFactory factory = HibernateUtility.getSessionFactory();
+		Session session = factory.openSession();
+		Transaction tx = null;
+		try {
+
+			tx = session.beginTransaction();
+			session.update(employee);
+			tx.commit();
+
+		} catch (Exception e) {
+			if (tx != null)
+				tx.rollback();
+			throw e;
+		} finally {
+			session.close();
+		}
+	}
+
+	@Override
+	public List<Employee> findAllEmployee() {
+		SessionFactory factory = HibernateUtility.getSessionFactory();
+		Session session = factory.openSession();
+		String hql = "from Employee e";
+		Query<Employee> query = session.createQuery(hql);
+		return query.list();
+
 	}
 
 }
