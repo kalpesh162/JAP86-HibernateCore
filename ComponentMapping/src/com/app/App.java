f44e977b45d4c0c5d74200cf556e8c6d8cf4c856
kalpesh <kalpesh162@gmail.com>
2026-05-14 16:29:01 +0530

    component mapping update and merge

--- ComponentMapping/src/com/app/App.java
+++ ComponentMapping/src/com/app/App.java
@@ -1,24 +1,60 @@
 package com.app;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
 import com.dao.EmployeeDao;
 import com.dao.EmployeeDaoImpl;
 import com.entity.Address;
 import com.entity.Employee;
+import com.utility.HibernateUtility;
 
 public class App {
 
 	public static void main(String[] args) {
+		/*
+		  Employee employee = new Employee(); Address address = new Address("FC ROAD",
+		  "PUNE", "MH", "411025"); employee.setName("Kalpesh");
+		  employee.setSalary(10000);
+		  
+		  employee.setAddress(address);
+		  
+		  EmployeeDao dao = new EmployeeDaoImpl(); dao.saveEmployee(employee);
+		 */
+		
+		/*
+		System.out.println("+++++++++++++++++++++++++++");
 
-		Employee employee = new Employee();
-		Address address = new Address("FC ROAD", "PUNE", "MH", "411025");
-		employee.setName("Kalpesh");
-		employee.setSalary(10000);
+		SessionFactory factory = HibernateUtility.getSessionFactory();
+		Session session1 = factory.openSession();
+		Employee employee1 = (Employee) session1.get(Employee.class, 1);
+		employee1.setName("Karsihma");
+		employee1.setSalary(23000);
 
-		employee.setAddress(address);
+		session1.evict(employee1); // employee1 is in Detached State
 
-		EmployeeDao dao = new EmployeeDaoImpl();
-		dao.saveEmployee(employee);
+		Session session2 = factory.openSession();
+		Transaction tx = session2.beginTransaction();
+		Employee employee2 = (Employee) session1.get(Employee.class, 1);
 
+		// Deatched --> session.update(obj) org.hibernate.NonUniqueObjectException
+		// session2.update(employee1); //
+		// session1.update(employee1); // org.hibernate.NonUniqueObjectException
+		System.out.println("((((((((((((((((((((((");
+		// session1.merge(employee1);
+		System.out.println("((((((((((((((((((((((");
+		// Detached State ----> PerSist State
+		session2.merge(employee1);
+
+		System.out.println("------------------------------");
+		tx.commit();
+		System.out.println("------------------------------");
+
+		System.out.println("*******");
+
+		System.out.println(session2.get(Employee.class, 1));
+	*/
 	}
 
 }
